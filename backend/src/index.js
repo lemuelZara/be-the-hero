bafb0c906b65cbbdc6f975c81e92fff7351b11fb
lemuelZara <lemuel_zara@outlook.com>
2020-03-29 12:36:09 -0300

    adicionando cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 
 const server = express()
 
+server.use(cors())
+/* 
+server.use(cors({
+    origin: 'http://meusite.com.br
+}))
+*/
 server.use(express.json())
 server.use(routes)
 
