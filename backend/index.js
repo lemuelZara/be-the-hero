c48dcdaeef93d5242429a085082327a9f8d54a18
lemuelZara <lemuel_zara@outlook.com>
2020-03-29 02:47:12 -0300

    Utilizando os conceitos do express

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 
 const server = express()
+server.use(express.json())
 
-server.listen(3333)
+/**
+ * GET: Buscar informações
+ * POST: Cadastrando uma informação
+ * PUT: Alterar uma informação
+ * DELETE: Deletar uma informação
+ */
+
+/**
+ * Query Params: request.query, enviados na rota após "?"
+ * Route Params: request.params, utilizados para identificar recursos
+ * Request Body: request.body, corpo da requisição (Alteração, criação)
+ */
 
 server.get('/', (req, res) => {
-    return res.json({message: 's'})
+    return res.json({ message: 's' })
 })
+
+server.listen(3333)
