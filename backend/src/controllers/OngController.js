46b3487e8f4778fc7c37b5ffab89b4ace22e4a7d
lemuelZara <lemuel_zara@outlook.com>
2020-03-29 03:51:49 -0300

    Criando os controllers para as Entidades da aplicação

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,28 @@
+const connection = require('../database/connection')
+
+const crypto = require('crypto')
+
+module.exports = {
+    async store(request, response) {
+        const id = crypto.randomBytes(4).toString('HEX')
+
+        const { name, email, whatsapp, city, uf } = request.body
+
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        })
+
+        return response.json({ id })
+    },
+
+    async show(request, response) {
+        const ongs = await connection('ongs').select('*')
+
+        return response.json(ongs)
+    }
+}
