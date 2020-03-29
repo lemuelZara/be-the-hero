509f3c568cd7dc6180953d01d5f7ba0121bf1b6b
lemuelZara <lemuel_zara@outlook.com>
2020-03-29 03:25:29 -0300

    Database Incidents

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: '.src/database/db.sqlite'
+      filename: './src/database/db.sqlite'
     },
     migrations: {
       directory: './src/database/migrations'
