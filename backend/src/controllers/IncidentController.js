afc7ba90e54e532c4b4a763267f6e2f088d913b7
lemuelZara <lemuel_zara@outlook.com>
2020-03-29 04:22:51 -0300

    Adicionando método de exclusão

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -23,5 +23,19 @@ module.exports = {
 
     async delete(request, response) {
         const { id } = request.params
+        const ong_id = request.headers.authorization
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first()
+
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not permited.'})
+        }
+
+        await connection('incidents').where('id', id).delete()
+
+        return response.status(204).send()
     }
 }
