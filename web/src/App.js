17d2262b68f39d42a0eb2eb8a95613b3b92bbbe2
lemuelZara <lemuel_zara@outlook.com>
2020-03-31 20:53:48 -0300

    aplicando estilos globais

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import { Logon } from './pages/Logon/index'
+import { Routes } from './routes'
 
 import './global.css'
 
 export const App = () => {
   return (
-    <Logon />
+    <Routes />
   )
 }
