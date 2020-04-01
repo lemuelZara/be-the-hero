d8b4c223e086858ddc6acdf1b6e0d921112a1e7b
lemuelZara <lemuel_zara@outlook.com>
2020-03-31 21:32:22 -0300

    adicionando página de pefil do usuário

--- web/src/routes.js
+++ web/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Logon } from './pages/Logon/index'
 import { Register } from './pages/Register/index'
+import { Profile } from './pages/Profile/index'
 
 export const Routes = () => {
     return (
@@ -10,6 +11,7 @@ export const Routes = () => {
             <Switch>
                 <Route path="/" component={Logon} exact />
                 <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
             </Switch>
         </BrowserRouter>
     )
