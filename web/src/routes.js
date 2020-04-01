b3b9d1fba746c72e0354d450aa7183897c54ecac
lemuelZara <lemuel_zara@outlook.com>
2020-03-31 21:53:24 -0300

    adicionando nas rotas a página de incidentes

--- web/src/routes.js
+++ web/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Logon } from './pages/Logon/index'
 import { Register } from './pages/Register/index'
 import { Profile } from './pages/Profile/index'
+import { NewIncident } from './pages/NewIncident/index'
 
 export const Routes = () => {
     return (
@@ -12,6 +13,7 @@ export const Routes = () => {
                 <Route path="/" component={Logon} exact />
                 <Route path="/register" component={Register} />
                 <Route path="/profile" component={Profile} />
+                <Route path="/incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     )
