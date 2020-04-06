0c7068f790a1b7b9bb943dd8cbf7944a05cea817
lemuelZara <lemuel_zara@outlook.com>
2020-04-06 09:45:01 -0300

    serviço de logon e salvando no localStorage os dados de logon

--- web/src/pages/Logon/index.js
+++ web/src/pages/Logon/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { api } from '../../services/api'
 
 import './styles.css'
 
@@ -9,15 +10,37 @@ import heroesImg from '../../assets/heroes.png'
 import logoImg from '../../assets/logo.svg'
 
 export const Logon = () => {
+    const [id, setId] = useState('')
+
+    const history = useHistory()
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        try {
+            const response = await api.post('/sessions', { id })
+
+            localStorage.setItem('ongId', id)
+            localStorage.setItem('ongName', response.data.name)
+
+            history.push('/profile')
+        } catch (error) {
+            alert(`Erro: ${error}`)
+        }   
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={event => setId(event.target.value)} />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
